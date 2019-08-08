405613be3dc849baa902c89951e7dea791a8bfe4
thebeardphantom <michael@beardphantom.com>
2019-08-08 02:11:37 -0700

    Added AudioCue previewing in editor

---
+++ UnityCommonLibrary.Editor/EditorAudioUtility.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using BeardPhantom.UCL.Assets;
+using UnityEditor;
+using UnityEngine;
+
+namespace BeardPhantom.UCL.Editor
+{
+    [InitializeOnLoad]
+    public static class EditorAudioUtility
+    {
+        #region Fields
+
+        private static readonly List<AudioSource> _playingSources = new List<AudioSource>();
+
+        #endregion
+
+        #region Constructors
+
+        static EditorAudioUtility()
+        {
+            EditorApplication.update -= EditorUpdate;
+            EditorApplication.update += EditorUpdate;
+
+            EditorApplication.playModeStateChanged += OnPlaymodeStateChanged;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public static AudioSource Play(AudioCueAsset cue)
+        {
+            var source = GetAudioSource();
+            cue.Play(source);
+            return source;
+        }
+
+        private static void OnPlaymodeStateChanged(PlayModeStateChange obj)
+        {
+            for (var i = _playingSources.Count - 1; i >= 0; i--)
+            {
+                var source = _playingSources[i];
+                if (source != null)
+                {
+                    Object.DestroyImmediate(source.gameObject);
+                }
+
+                _playingSources.RemoveAt(i);
+            }
+        }
+
+        private static AudioSource GetAudioSource()
+        {
+            var audioSource = UnityEditor.EditorUtility.CreateGameObjectWithHideFlags(
+                    "AUDIOSOURCE",
+                    HideFlags.HideAndDontSave,
+                    typeof(AudioSource))
+                .GetComponent<AudioSource>();
+            _playingSources.Add(audioSource);
+            return audioSource;
+        }
+
+        private static void EditorUpdate()
+        {
+            for (var i = _playingSources.Count - 1; i >= 0; i--)
+            {
+                var source = _playingSources[i];
+                var nullSource = source == null;
+                if (!nullSource && source.isPlaying)
+                {
+                    continue;
+                }
+
+                if (!nullSource)
+                {
+                    Object.DestroyImmediate(source.gameObject);
+                }
+
+                _playingSources.RemoveAt(i);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ UnityCommonLibrary.Editor/Inspectors/AudioCueAssetEditor.cs
@@ -0,0 +1,49 @@
+﻿using BeardPhantom.UCL.Assets;
+using UnityEditor;
+using UnityEngine;
+
+namespace BeardPhantom.UCL.Editor.Inspectors
+{
+    [CustomEditor(typeof(AudioCueAsset))]
+    public class AudioCueAssetEditor : UnityEditor.Editor
+    {
+        #region Fields
+
+        private AudioSource _lastPlayingSource;
+
+        #endregion
+
+        #region Methods
+
+        /// <inheritdoc />
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            var cachedColor = GUI.color;
+            if (_lastPlayingSource == null)
+            {
+                GUI.color = Color.green;
+                if (GUILayout.Button("Play"))
+                {
+                    _lastPlayingSource = EditorAudioUtility.Play((AudioCueAsset) target);
+                }
+            }
+            else
+            {
+                GUI.color = Color.red;
+                if (GUILayout.Button("Stop"))
+                {
+                    _lastPlayingSource.Stop();
+                }
+            }
+
+            GUI.color = cachedColor;
+
+            DrawPropertiesExcluding(serializedObject, "m_Script");
+            serializedObject.ApplyModifiedProperties();
+            Repaint();
+        }
+
+        #endregion
+    }
+}
