67a94c6d3bf5dd66e3c1f323912e2d65e50e3d2f
thebeardphantom <michael@beardphantom.com>
2019-08-11 19:54:59 -0700

    Added raw image to PanTexture. Fixed using non-instanced material in editor.

--- UnityCommonLibrary/PanTexture.cs
+++ UnityCommonLibrary/PanTexture.cs
@@ -33,10 +33,27 @@ namespace BeardPhantom.UCL
                 return;
             }
 
+            var rawImage = GetComponent<RawImage>();
+            if (rawImage)
+            {
+#if UNITY_EDITOR
+                _material = Instantiate(rawImage.material);
+                rawImage.material = _material;
+#else
+                _material = rawImage.material;
+#endif
+                return;
+            }
+
             var image = GetComponent<Image>();
             if (image)
             {
+#if UNITY_EDITOR
+                _material = Instantiate(image.material);
+                image.material = _material;
+#else
                 _material = image.material;
+#endif
             }
         }
 
