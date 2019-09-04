318d1549d73bf2f3b94af30d55e66eb34fc48790
thebeardphantom <michael@beardphantom.com>
2019-09-03 22:36:00 -0700

    More bootstrapper bugfixes

--- UnityCommonLibrary/Bootstrapper.cs
+++ UnityCommonLibrary/Bootstrapper.cs
@@ -69,6 +69,7 @@ namespace UnityCommonLibrary
             if (EnsureInstance())
             {
                 StoreDesiredScenes();
+                SceneManager.LoadScene(_bootstrapSceneBuildIndex);
             }
         }
 
