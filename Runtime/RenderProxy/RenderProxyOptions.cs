b772f8feed18cea7bbfff91f531774ce4588b0a9
Michael Notarnicola <michael@beardphantom.com>
2020-02-23 15:18:49 -0800

    Added RenderProxy

---
+++ Runtime/RenderProxy/RenderProxyOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BeardPhantom.UCL
+{
+    [Flags]
+    public enum RenderProxyOptions
+    {
+        UseUniqueMeshInstance = 1 << 0,
+        UseUniqueMaterialInstance = 1 << 1,
+        LogUnsupportedRendererTypes = 1 << 2,
+        FindInactiveRenderers = 1 << 3,
+    }
+
+    public static class RenderProxyOptionsExtensions
+    {
+        #region Methods
+
+        public static bool HasFlagFast(this RenderProxyOptions value, RenderProxyOptions flag)
+        {
+            return (value & flag) != 0;
+        }
+
+        #endregion
+    }
+}
