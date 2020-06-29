b000d9073cdd2ce96fc183e459ccd97434e2c09f
Michael Notarnicola <michael@beardphantom.com>
2020-06-28 17:21:20 -0700

    Better event bus process limits

---
+++ Runtime/EventBus/EventBusProcessLimit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BeardPhantom.UCL
+{
+    [Serializable]
+    public abstract class EventBusProcessLimit
+    {
+        #region Methods
+
+        public abstract bool CanContinue(int processedEventCount, TimeSpan timeSpentProcessing);
+
+        #endregion
+    }
+}
---
+++ Runtime/EventBus/UnlimitedProcessLimit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BeardPhantom.UCL
+{
+    public class UnlimitedProcessLimit : EventBusProcessLimit
+    {
+        #region Methods
+
+        /// <inheritdoc />
+        public override bool CanContinue(int processedEventCount, TimeSpan timeSpentProcessing)
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
